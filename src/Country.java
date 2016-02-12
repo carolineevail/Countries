ea2a7fed38fd494917b0919b040f5ffa16a93508
carolineevail <carolineevail@gmail.com>
2016-02-11 21:10:34 -0500

    Created a reader and scanner.

---
+++ src/Country.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Caroline on 2/11/16.
+ */
+public class Country {
+    String countryName;
+    String countryAbbreviation;
+
+    public Country (String countryName, String countryAbbreviation){
+
+    }
+}
